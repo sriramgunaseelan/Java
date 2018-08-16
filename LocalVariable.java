7998fb2f0921d7bc16a20546fe8169a4f444de00
sriram <sriramcg1509@gmail.com>
2018-08-16 15:37:50 +0530

    2

---
+++ LocalVariable.java
@@ -0,0 +1,8 @@
+//Local Variables
+
+class LocalVariable{
+  public static void main(String args[]){
+	int a = 5;                            //local variable , scope = within the block.
+	System.out.println(a);
+	}
+   }
