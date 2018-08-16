5b9cfdb41d378d606fa02b1c5a58a46b866d9dea
sriram <sriramcg1509@gmail.com>
2018-08-16 15:33:46 +0530

    2

---
+++ StaticVariable.java
@@ -0,0 +1,12 @@
+//static variables.
+
+class StaticVariable{
+	
+	static int num = 5;
+	
+	//we can access static variables without objects
+   public static void main(String args[]){
+	int a = 2;
+	System.out.println(num+a);
+	}
+}
